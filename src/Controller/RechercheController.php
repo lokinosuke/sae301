520bdc0cba6d9a8b8fa5810349aeed0bf64cb01d
lokinosuke <nourhane.boudaoud@etudiant.univ-reims.fr>
2022-12-02 13:23:05 +0100

    modif navbar ajout dropdown

--- src/Controller/RechercheController.php
+++ src/Controller/RechercheController.php
@@ -2,7 +2,10 @@
 
 namespace App\Controller;
 
+use App\Entity\Manif;
 use App\Form\RechercheType;
+use App\Repository\ManifRepository;
+use ContainerX76XxQl\getManifService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -12,7 +15,7 @@ class RechercheController extends AbstractController
     #[Route('/recherche', name: 'app_recherche')]
     public function index(): Response
     {
-        $form = $this->createForm(Manif::class);
+        $form = $this->createForm(ManifRepository::class);
 
         return $this->render('/recherche/index.html.twig', [
             'form' => $form->createView()
