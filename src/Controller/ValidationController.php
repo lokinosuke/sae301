8c1ddc0cf046172cdbe3f4c7c1f933970c4e07d2
lokinosuke <nourhane.boudaoud@etudiant.univ-reims.fr>
2022-12-04 16:43:17 +0100

    mentions legales

--- src/Controller/ValidationController.php
+++ src/Controller/ValidationController.php
@@ -6,13 +6,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class FinalisationController extends AbstractController
+class ValidationController extends AbstractController
 {
-    #[Route('/finalisation', name: 'app_finalisation')]
+    #[Route('/validation', name: 'app_finalisation')]
     public function index(): Response
     {
-        return $this->render('finalisation/index.html.twig', [
-            'controller_name' => 'FinalisationController',
+        return $this->render('validation/index.html.twig', [
+            'controller_name' => 'ValidationController',
         ]);
     }
 }
